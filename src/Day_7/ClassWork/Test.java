3257d3f9548922d7bf99044b047337ead239f62f
Pavithra <pavithraarumugam925@gmail.com>
2026-06-27 15:14:08 +0530

    Program created

---
+++ src/Day_7/ClassWork/Test.java
@@ -0,0 +1,11 @@
+package Day_7.ClassWork;
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name = sc.nextLine();
+        System.out.print("Enter your age:");
+        int age = sc.nextInt();
+    }
+}
