03354cde3921779899f704eb8ff213dc52152b85
Pavithra <pavithraarumugam925@gmail.com>
2026-06-28 21:31:30 +0530

    Program to print 10 consecutive numbers reversely

---
+++ src/Day_4/ClassTask/ForLoop.java
@@ -0,0 +1,9 @@
+package Day_4.ClassTask;
+
+public class ForLoop{
+    public static void main(String[] args){
+        for(int i=10;i>=1;i--){
+            System.out.println("Hello "+ i);
+        }
+    }
+}
