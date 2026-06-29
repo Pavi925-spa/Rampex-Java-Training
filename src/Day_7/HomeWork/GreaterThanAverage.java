8d695ee069ac1c53cfeead950b467922a3bf507e
Pavithra <pavithraarumugam925@gmail.com>
2026-06-29 09:15:26 +0530

    Program to find the greater number than Average

---
+++ src/Day_7/HomeWork/GreaterThanAverage.java
@@ -0,0 +1,19 @@
+package Day_7.HomeWork;
+
+public class GreaterThanAverage {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        double average = (double) sum / arr.length;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > average)
+                count++;
+        }
+        System.out.println("Average: " + average);
+        System.out.println("Count: " + count);
+    }
+}
