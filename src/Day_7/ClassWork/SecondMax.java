cc7a6d04b3cbfb3f41d5b793e979b06eb98ae51c
Pavithra <pavithraarumugam925@gmail.com>
2026-06-29 10:22:00 +0530

    Program to rotate the string array

--- src/Day_7/ClassWork/SecondMax.java
+++ src/Day_7/ClassWork/SecondMax.java
@@ -4,7 +4,7 @@ public class SecondMax {
     public static void main(String[] args){
         int secondmax = 0;
         int max = 0;
-        int [] arr = {3,5,7,8,9};
+        int [] arr = {1,2,3,4,5};
         for(int i = 0;i<arr.length;i++){
             if(arr[i] > max){
                 secondmax = max;
