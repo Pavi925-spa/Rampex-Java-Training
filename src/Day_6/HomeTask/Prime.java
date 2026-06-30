084463616537b78f9997d53e0e349b61963266a5
Pavithra <pavithraarumugam925@gmail.com>
2026-06-30 11:36:33 +0530

    prime number

--- src/Day_6/HomeTask/Prime.java
+++ src/Day_6/HomeTask/Prime.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Prime {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number:");
+        System.out.print("Enter a number: ");
         int n = sc.nextInt();
         int count = 0;
         int num = 2;
